dd0af2d268e20a27e83fa339768ea80078708d13
[cloud4] 이은주 <silverzoo030@gmail.com>
2024-07-09 17:40:24 +0900

    :sparkles: ch0302 - java.lang.Math

---
+++ [ch0302]Generics_pakages/src/TestMathRound.java
@@ -0,0 +1,7 @@
+public class TestMathRound {
+    public static void main(String[] args) {
+        double value = 9.5;
+        Long rounded = Math. round (value);
+        System.out.println(rounded); //가장 가까운 정수로 반올림하는 메서드
+    }
+}
