007430932fe54a6dbf27faaf838782c222b50a52
[cloud4] 이은주 <silverzoo030@gmail.com>
2024-07-24 17:41:04 +0900

    :sparkles: ch0503 - sql(dml)

--- [ch0503]sql/src/12서적.sql
+++ [ch0503]sql/src/12서적.sql
@@ -1,6 +1,17 @@
+-- 방식 1.
 SELECT MAX(price) as 최댓값, MIN(price) as 최솟값
 FROM books
 WHERE publication_year BETWEEN 2001 AND 2003;
+
+-- 방식 2.
+WITH filtered_books as (
+    SELECT price
+    FROM books
+    WHERE publication_year BETWEEN 2001 AND 2003
+)
+
+SELECT MAX(price) as 최댓값, MIN(price) as 최솟값
+FROM filtered_books;
 
 -- 제대로 출력했는지 price를 오름차순으로 확인
 SELECT *
